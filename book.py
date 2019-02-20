24ca0d0e9a16f6d11aacb4915ca1e682e5d86c36
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-20 15:24:49 -0500

    added the create class method on Book

--- book.py
+++ book.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 class Book:
+    """Represents a collection of books that are either on the shelves, or
+    that have been loaned out
+    """
     on_shelf = []
     on_loan = []
 
@@ -8,3 +11,11 @@ class Book:
         self.title = title
         self.author = author
         self.isbn = isbn
+
+    @classmethod
+    def create(cls,title,author,isbn):
+        new_book = Book(title,author,isbn)
+        Book.on_shelf.append(new_book)
+        return new_book
+
+print(Book.create('book','author','isbn'))
