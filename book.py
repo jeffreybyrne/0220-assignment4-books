1458ff1e2f5d43dde9a5862df54f0d39f2856711
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-20 15:20:51 -0500

    added an init function

--- book.py
+++ book.py
@@ -3,3 +3,8 @@ from datetime import datetime
 class Book:
     on_shelf = []
     on_loan = []
+
+    def __init__(self,title,author,isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
